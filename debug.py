ac901e1a5ed79c828280487a53182694075e1a8e
Leon Hecht <sendmail2leon@gmail.com>
2025-08-24 13:43:50 +0000

    Initial commit with two big projects.

---
+++ debug.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/<path:path>')
+def send_static(path):
+    return send_from_directory('.', path)
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
